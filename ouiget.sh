2ddd0957e7023b2711ab7f30ab49f06f858595b6
cockcut <105777311+cockcut@users.noreply.github.com>
2025-09-14 21:20:21 +0900

    Update ouiget.sh

--- ouiget.sh
+++ ouiget.sh
@@ -1,52 +1,52 @@
 #!/bin/bash
 
-# Install necessary programs
-sudo dnf -y install wget httpd php php-json php-curl jq php-snmp net-snmp-utils php-mbstring php-xml expect php-fpm git
-
-
-# Grant execute permissions to *.sh scripts in the current directory and subdirectories
-find . -name "*.sh" -exec chmod +x {} \;
-# Grant execute permissions to directories
-chmod -R 777 api-u
-
-# Related to oui script below
-# Dynamically get the actual path of the ouiget.sh script.
-# 'readlink -f' finds the absolute path of the script, including symbolic links.
-# 'dirname' extracts only the directory part from that absolute path.
+# ouiget.sh 스크립트의 절대 경로를 동적으로 가져와 변수에 저장합니다.
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-OUIGET_SCRIPT="$SCRIPT_DIR/ouiget.sh"
 
-# Grant execute permissions to ouiget.sh
-chmod +x "$OUIGET_SCRIPT"
+# 먼저 기존에 남아있는 OUI파일을 삭제합니다.
+rm -f "$SCRIPT_DIR/oui/index.html"*
 
-# Define job to add to crontab (to be executed at 00:00 every day)
-CRON_JOB="0 0 * * * $OUIGET_SCRIPT"
+# OUI를 디렉토리에 저장합니다.
+wget https://standards-oui.ieee.org/ -P "$SCRIPT_DIR/oui/"
 
-# Get the current crontab list
-(crontab -l 2>/dev/null) > my_crontab_jobs
+# OUI 파일의 내용을 읽고, "(hex)"가 포함된 줄을 필터링합니다.
+cat "$SCRIPT_DIR/oui/index.html" | \
+grep hex | \
 
-# Check if the job already exists
-if ! grep -qF -- "$CRON_JOB" my_crontab_jobs; then
-  # If the job does not exist, add it
-  echo "$CRON_JOB" >> my_crontab_jobs
+# "파싱을 용이하게 하기 위해 (hex)"를 구분자 "|"로 변경합니다.
+sed 's/(hex)/|/g' | \
 
-  # Install the new crontab list
-  crontab my_crontab_jobs
-  echo "ouiget.sh has been successfully added to crontab."
-else
-  echo "ouiget.sh is already added to crontab. Skipping."
-fi
+# awk를 사용하여 구분자 "|"로  열을 구분하고 데이터 형식을 지정합니다.
+awk -F '|' -v timestamp="$(date +"%Y/%m/%d/%H:%M")" '
 
-# Delete temporary file
-rm my_crontab_jobs
+# 시작 시, 헤더와 타임스탬프를 출력합니다.
+  BEGIN {
+    print "*" timestamp " 업데이트\n"
+#    print "OUI\t\tOUI(:)\t\tOUI(lower)\t\tOUI(:lower)\t\t\t\tVender"
+    print "OUI\t\tOUI(:)\t\tOUI(lower)\tOUI(:lower)\t\t\tVender"
+    print "------------------------------------------------------------------------------------------------------------------------------------------"
+  }
 
-# Increase file upload capacity in php.ini
-sudo sed -i 's/^upload_max_filesize = 2M/upload_max_filesize = 100M/' /etc/php.ini
+# 각 줄을 처리합니다.
+  {
+# 원본 OUI 값을 변수에 저장합니다.
+    original_oui = $1
+# 콜론으로 변경된 OUI 값을 생성합니다.
+    colon_oui = original_oui
+    gsub("-", ":", colon_oui)
+# 소문자로 변경된 OUI 값을 생성합니다.
+    lower_oui = tolower(original_oui)
+# 콜론으로 변경된 OUI 값을 소문자로 변환합니다.
+    lower_colon_oui = tolower(colon_oui)
+    
+# 총 5개의 열을 출력합니다.
+#    printf "%s\t\t%s\t\t%s\t\t%s\t\t%s\n", original_oui, colon_oui, lower_oui, lower_colon_oui, $2
+    printf "%s\t%s\t%s\t%s\t%s\n", original_oui, colon_oui, lower_oui, lower_colon_oui, $2
+  }
+# 끝날 때, 푸터를 출력합니다.
+  END {
+    print "------------------------------------------------------------------------------------------------------------------------------------------\nEND"
+  }
+' > $SCRIPT_DIR/oui/oui.txt
+rm -f $SCRIPT_DIR/oui/index.html*
 
-# Turn on httpd service
-sudo systemctl enable httpd
-sudo systemctl restart httpd
-
-# Open firewall
-sudo firewall-cmd --permanent --add-service=http
-sudo firewall-cmd --reload
